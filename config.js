7f48ce487cfc9f06950e922d98b4557270542362
Fourzy <94512377+Fourzylol@users.noreply.github.com>
2022-03-03 17:39:18 +0700

    Update config.js

--- config.js
+++ config.js
@@ -48,8 +48,8 @@ global.wait = '「 ⏱️ 」Harap tunggu...'
 global.eror = '「❗」Server ERROR!'
 global.fla = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&fillTextPattern=Warning!&text='
 global.watermark = '©Fourzy' //change the watermark 
-global.image = 'https://telegra.ph/file/3d470e0949899403bddeb.jpg' //change the image
-global.thumbfoto = 'https://telegra.ph/file/a61818adfcb3f67bba9c5.jpg'
+global.image = 'https://telegra.ph/file/b0eafadef41eb1b656c2b.jpg' //change the image
+global.thumbfoto = 'https://telegra.ph/file/daddfc55c6aa222d68358.jpg'
 
 // image
 global.bank = 'https://telegra.ph/file/d5ddf4cc627bb0e6bc420.jpg'
