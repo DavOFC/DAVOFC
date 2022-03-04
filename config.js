8938615eb4f61945b8ab451845c38285e4702000
Fourzy <94512377+Fourzylol@users.noreply.github.com>
2022-03-04 15:38:35 +0700

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@
 
 
 let fs = require('fs')
-global.owner = ['6285748600473', '994406397868'] // Letakan nomor kamu disini
+global.owner = ['6285748600473'] // Letakan nomor kamu disini
 global.APIs = { // API Prefix
   // nama: 'https://website'
   hardianto: 'https://hardianto-chan.herokuapp.com',
